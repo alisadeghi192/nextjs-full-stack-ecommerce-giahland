1d52d0b9179f33a71d62d08506146a1521ff7621
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-02-21 15:42:48 +0330

    feat(landing): complete responsive header with static components

---
+++ src/app/components/shared/ui/Icon-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface IconButtonProps {
+  icon: React.ReactNode;
+  onClick?: () => void;
+}
+
+export default function IconButton({ icon, onClick }: IconButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`border-primary group hover:border-shade2 flex size-8 cursor-pointer items-center justify-center rounded-lg border transition-colors sm:size-10 sm:rounded-xl md:size-12`}
+    >
+      <span className="text-primary group-hover:text-shade2 transition-colors">
+        {icon}
+      </span>
+    </button>
+  );
+}
