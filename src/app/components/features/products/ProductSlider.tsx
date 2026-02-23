59d277a031189948d56334324030a203c213722a
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-02-23 12:03:08 +0330

    fix(products): resolve responsive bug in product slider

--- src/app/components/features/products/ProductSlider.tsx
+++ src/app/components/features/products/ProductSlider.tsx
@@ -44,11 +44,12 @@ export default function ProductSlider({ title, products }: ProductSliderProps) {
             pauseOnMouseEnter: true,
           }}
           breakpoints={{
-            0: { slidesPerView: 1, spaceBetween: 16 },
-            320: { slidesPerView: 1, spaceBetween: 16 },
+            0: { slidesPerView: 1.25, spaceBetween: 16 },
+            320: { slidesPerView: 1.25, spaceBetween: 16 },
+            350: { slidesPerView: 1.5, spaceBetween: 16 },
             380: { slidesPerView: 1.5, spaceBetween: 16 },
             400: { slidesPerView: 1.75, spaceBetween: 16 },
-            500: { slidesPerView: 2, spaceBetween: 16 },
+            500: { slidesPerView: 2.25, spaceBetween: 16 },
             600: { slidesPerView: 2.5, spaceBetween: 16 },
             640: { slidesPerView: 2, spaceBetween: 16 },
             768: { slidesPerView: 3, spaceBetween: 20 },
