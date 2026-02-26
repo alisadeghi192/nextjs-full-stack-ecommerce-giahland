3dd2906edc2d0833b78db5a6701d0cac0a721da7
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-02-26 18:04:27 +0330

    feat(ui): add custom 404 page with responsive design

---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import PublicHeader from "@@/components/layout/public/PublicHeader";
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <main className="font-modam min-h-dvh ">
+      <div className="absolute top-0 left-0 right-0">
+        <PublicHeader />
+      </div>
+      <div className="flex max-sm:flex-col-reverse items-center min-h-dvh justify-center basis-1/2 px-4 text-center">
+        <div>
+          <h1 className="text-primary text-8xl font-black max-sm:text-5xl">۴۰۴</h1>
+          <h2 className="text-neutral11 mt-5 text-2xl font-semibold max-sm:text-xl">
+            صفحه مورد نظر یافت نشد.
+          </h2>
+          <p className="text-neutral9 mt-3 max-w-120">
+            متأسفیم! صفحه‌ای که به دنبال آن هستید وجود ندارد یا ممکن است حذف شده
+            باشد.
+          </p>
+          <Link
+            href="/"
+            className="bg-primary text-WHITE mt-5 flex flex-1 cursor-pointer items-center justify-center rounded-xl py-3 w-9/10 mx-auto"
+          >
+            <span className="text-lg/8 max-md:text-sm max-md:font-medium">
+              بازگشت به صفحه اصلی
+            </span>
+          </Link>
+        </div>
+        <Image alt="404" src="/404.png" width={500} height={500} className="max-lg:size-100 max-md:size-80 aspect-square"  />
+      </div>
+    </main>
+  );
+}
