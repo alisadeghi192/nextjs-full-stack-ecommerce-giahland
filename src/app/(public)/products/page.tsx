8bb49e705145cb7846aa29fed8093a9d0edc706a
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-11 20:44:24 +0330

    feat(products): add products page with tabs and breadcrumb

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import Breadcrumb from '@@/components/shared/ui/Breadcrumb'
+'use client'
+import React, { useState } from "react";
+import Breadcrumb from "@@/components/shared/ui/Breadcrumb";
 
 function page() {
+  interface productsTab {
+    id: string;
+    label: string;
+  }
+  const tabs: productsTab[] = [
+    { id: "indoor", label: "گیاهان آپارتمانی" },
+    { id: "decoration", label: "گیاهان تزئینی" },
+    { id: "gift", label: "گیاهان کادویی" },
+  ];
+  const [activeTab, setActiveTab] = useState<string>(tabs[0].id);
+  const handleTabClick = (tabId: string) => {
+    setActiveTab(tabId);
+  };
   return (
-    <main className='container'>   
-        <Breadcrumb/>
+    <main className="container">
+      <Breadcrumb />
+      <section>
+        <div>
+          <div>
+            {tabs.map((tab) => (
+              <button
+                key={tab.id}
+                className={`text-neutral9 border-neutral5 cursor-pointer border-b p-3 text-2xl/6 font-normal transition-all ${activeTab === tab.id ? 'font-bold! text-primary! border-b-2! border-primary!' : ''}`}
+                onClick={()=> handleTabClick(tab.id)}
+              >
+                {tab.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
 
-export default page
+export default page;
