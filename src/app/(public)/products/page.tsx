d4414e242a6f2173fc13f3aed2e6ffe3d58d7f98
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-12 16:49:12 +0330

    feat(products): add grid/list toggle with state

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,22 +1,57 @@
-'use client'
+"use client";
+import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import Breadcrumb from "@@/components/shared/ui/Breadcrumb";
 import ProductsTabs from "@@/components/features/products/ProductsTabs";
+import { MdDensityMedium, MdGridView } from "react-icons/md";
+import { BsSortDownAlt } from "react-icons/bs";
 
 export default function ProductsPage() {
   const searchParams = useSearchParams();
   const activeTab = searchParams.get("category") || "indoor";
-  console.log(activeTab)
+
+  const [viewMode, setViewMode] = useState<"grid" | "list">("grid");
 
   return (
     <main className="container">
       <Breadcrumb />
       <section>
-        <div>
-          <ProductsTabs activeTab={activeTab as "indoor" | "decoration" | "gift"} />
+        <div className="flex items-center justify-between">
+          <ProductsTabs
+            activeTab={activeTab as "indoor" | "decoration" | "gift"}
+          />
+
+          <div className="flex items-center gap-x-4">
+            <button
+              onClick={() => setViewMode("grid")}
+              className={`border-neutral5 flex size-12 items-center justify-center rounded-xl border transition-all ${
+                viewMode === "grid"
+                  ? "bg-primary text-white"
+                  : "bg-neutral2 text-primary"
+              }`}
+            >
+              <MdGridView className="size-6" />
+            </button>
+
+            <button
+              onClick={() => setViewMode("list")}
+              className={`border-neutral5 flex size-12 items-center justify-center rounded-xl border transition-all ${
+                viewMode === "list"
+                  ? "bg-primary text-white"
+                  : "bg-neutral2 text-primary"
+              }`}
+            >
+              <MdDensityMedium className="size-6" />
+            </button>
+
+            <button className="bg-neutral2 border-neutral5 text-primary flex h-12 w-36 cursor-pointer items-center justify-center gap-x-2 rounded-xl border">
+              <BsSortDownAlt className="size-6" />
+              <span>مرتب سازی</span>
+            </button>
+          </div>
         </div>
-        <div className="mt-8">
-        </div>
+
+        <div className="mt-8"></div>
       </section>
     </main>
   );
