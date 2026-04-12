e279e27884bd97d78b72cfdeeff310bec29d0c0b
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-12 19:51:35 +0330

    fix(products): fix change view and sort when switching tabs bug

--- src/app/components/features/products/ProductsTabs.tsx
+++ src/app/components/features/products/ProductsTabs.tsx
@@ -1,8 +1,10 @@
+'use client'
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 interface ProductTabsItems {
   id: 'indoor' | 'decoration' | 'gift';
-  label: string; 
+  label: string;
 }
 
 const tabs: ProductTabsItems[] = [
@@ -16,12 +18,25 @@ interface ProductsTabsProps {
 }
 
 export default function ProductsTabs({ activeTab }: ProductsTabsProps) {
+  const searchParams = useSearchParams();
+  
+  const view = searchParams.get("view") || "grid";
+  const sort = searchParams.get("sort") || "newest";
+
+  const buildLink = (tabId: string) => {
+    const params = new URLSearchParams();
+    params.set("category", tabId);
+    params.set("view", view);
+    params.set("sort", sort);
+    return `?${params.toString()}`;
+  };
+
   return (
     <div className="flex">
       {tabs.map((tab) => (
         <Link
           key={tab.id}
-          href={`?category=${tab.id}`}
+          href={buildLink(tab.id)}
           className={`border-neutral5 cursor-pointer border-b p-3 text-2xl/6 transition-all ${
             activeTab === tab.id
               ? "font-bold text-primary border-b-2 border-primary"
