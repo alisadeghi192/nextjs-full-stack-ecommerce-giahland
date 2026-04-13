d9078fcf5abe528bb903c717794f1c5982d79e61
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-13 19:42:32 +0330

    feat(products): add ProductCardList component for list view

---
+++ src/components/features/products/ProductCardList.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductCardListProps {
+  name: string;
+  price: number;
+  image: string;
+  slug?: string;
+}
+
+export default function ProductCardList({
+  name,
+  price,
+  image,
+  slug = "/",
+}: ProductCardListProps) {
+  return (
+    <div className="border border-neutral5 rounded-lg p-4 flex">
+      <div className="rounded-lg overflow-hidden">
+        <Image alt={name} src={image} width={150} height={150}></Image>
+      </div>
+      <div className="grow justify-between flex flex-col">
+        <div className="flex flex-col ">
+          <Link href={slug}>
+            <p className=" text-lg/8">{name}</p>
+          </Link>
+          <p className="text-neutral9 leading-7">گیاه آپارتمانی</p>
+        </div>
+        <div className="flex justify-between  gap-x-6">
+          <div className="flex justify-between items-center basis-2/3">
+            <span className="text-sm/6.25 ">قیمت:</span>
+            <span className="text-lg/8">
+            {price.toLocaleString("fa-IR")} تومان
+            </span>
+          </div>
+          <Link href={slug} className="grow">
+            <button className="text-WHITE cursor-pointer bg-primary flex h-10 w-full items-center justify-center rounded-lg">
+              <span className="text-sm/5.5 font-medium">مشاهده بیشتر</span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
