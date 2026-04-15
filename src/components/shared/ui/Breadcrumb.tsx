db9a53b72b379fc75305ab398be4a81e58b67521
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-15 20:34:20 +0330

    style(ui): make Breadcrumb fully responsive

--- src/components/shared/ui/Breadcrumb.tsx
+++ src/components/shared/ui/Breadcrumb.tsx
@@ -13,12 +13,12 @@ export default function Breadcrumb() {
   const segments = pathname.split("/").filter(Boolean);
 
   return (
-    <nav className="flex items-center mt-6 mb-8">
+    <nav className="flex items-center mt-6 mb-8 max-lg:mt-4 max-lg:mb-4 max-md:mt-7 max-sm:mt-5  ">
       <Link
         href="/"
-        className="text-primary hover:text-shade2 flex items-center gap-x-1 text-lg/8 font-normal transition-colors"
+        className="text-primary hover:text-shade2 flex items-center gap-x-1 text-lg/8 max-md:text-base/6.25 max-xs:text-sm font-normal transition-colors"
       >
-        <MdOutlineHome className="size-6" />
+        <MdOutlineHome className="size-6 max-md:size-5 max-xs:size-4" />
         خانه
       </Link>
 
@@ -28,9 +28,9 @@ export default function Breadcrumb() {
 
         return (
           <div key={href} className="flex items-center gap-x-1">
-            <MdKeyboardArrowLeft className="size-6 text-neutral9" />
+            <MdKeyboardArrowLeft className="size-6 max-md:size-5 max-xs:size-4 text-neutral9 mr-1" />
             {isLast ? (
-              <span className=" text-neutral9 font-normal text-lg/8">
+              <span className=" text-neutral9 font-normal text-lg/8 max-md:text-base/6.25 max-xs:text-sm">
                 {nameMap[segment] || segment}
               </span>
             ) : (
