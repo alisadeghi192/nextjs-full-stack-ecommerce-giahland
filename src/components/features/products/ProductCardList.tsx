0c8477312314493a1b809c97b2648feea1948151
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-15 20:34:58 +0330

    refactor(products): major update to ProductCardList with stock, discount, dimensions, and like button

--- src/components/features/products/ProductCardList.tsx
+++ src/components/features/products/ProductCardList.tsx
@@ -1,44 +1,87 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Product } from "@/features/products/types/product.types";
-
+import { formatDimensions } from "@/lib/utils/formatDimensions";
+import { formatPrice } from "@/lib/utils/formatPrice";
+import { getDiscountedPrice } from "@/features/products/utils/productHelpers";
+import { useState } from "react";
+import { BsHeart, BsHeartFill } from "react-icons/bs";
 
 export default function ProductCardList({
   name,
   price,
   image,
   slug = "/",
-  category,
-  discount
+  discount,
+  potDimensions,
+  stock,
 }: Product) {
-  return (
-    <div className="border border-neutral5 rounded-lg p-4 flex gap-x-6">
-      <div className="rounded-lg overflow-hidden">
-        <Image alt={name} src={image} width={150} height={150} className="object-cover aspect-square rounded-lg"></Image>
-      </div>
-      <div className="grow justify-between flex flex-col">
-        <div className="flex flex-col ">
-          <Link href={slug}>
-            <p className=" text-lg/8">{name}</p>
-          </Link>
-          <p className="text-neutral9 leading-7">{category} </p>
-        <p>تخفیف: {discount}%</p>
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
+  const finalPrice = getDiscountedPrice(price, discount);
 
-        </div>
-        <div className="flex justify-between  gap-x-6">
-          <div className="flex justify-between items-center basis-2/3">
-            <span className="text-sm/6.25 ">قیمت:</span>
-            <span className="text-lg/8">
-            {price.toLocaleString("fa-IR")} تومان
-            </span>
+  return (
+    <div className="border relative border-neutral5 rounded-lg group overflow-hidden">
+      <Link href={slug}>
+        <div className=" p-4 flex gap-x-6">
+          <div className="rounded-lg overflow-hidden">
+            <Image
+              alt={name}
+              src={image}
+              width={120}
+              height={120}
+              className="object-cover aspect-square rounded-lg"
+            ></Image>
           </div>
-          <Link href={slug} className="grow">
-            <button className="text-WHITE cursor-pointer bg-primary flex h-10 w-full items-center justify-center rounded-lg">
-              <span className="text-sm/5.5 font-medium">مشاهده بیشتر</span>
-            </button>
-          </Link>
+          <div className="grow justify-between flex flex-col">
+            <div className="flex flex-col gap-y-1">
+              <p
+                className={`${stock === 0 && "text-neutral9"} text-lg/8 group-hover:text-primary transition color`}
+              >
+                {name}
+              </p>
+              <p className="text-sm/6.25 text-neutral9">
+                ابعاد: {formatDimensions(potDimensions)}
+              </p>
+            </div>
+            <div className="flex justify-end items-center gap-x-2">
+              {stock === 0 ? (
+                <span className="text-error text-lg/8">ناموجود</span>
+              ) : (
+                <>
+                  {discount > 0 && (
+                    <span className="relative text-sm/6.25 text-error">
+                      {formatPrice(price, false)}
+                      <span className="block absolute top-1/2 w-full h-px m-auto bg-error "></span>
+                    </span>
+                  )}
+                  <span className="text-lg/8 max-sm:text-base/7.25">
+                    {formatPrice(finalPrice)}
+                  </span>
+                </>
+              )}
+            </div>
+            {discount > 0 && (
+              <span className="font-bold text-xs/5.5 px-1 rounded-xl bg-bg-error absolute top-5 right-5 text-error">
+                {discount}%
+              </span>
+            )}
+          </div>
         </div>
-      </div>
+      </Link>
+      <button
+        className="absolute cursor-pointer top-4 -left-9 group-hover:left-4 size-8 rounded-full bg-bg-error flex items-center justify-center transition-all"
+        onClick={toggleLike}
+      >
+        {isLiked ? (
+          <BsHeartFill className="text-error size-5" />
+        ) : (
+          <BsHeart className="text-neutral7 size-5" />
+        )}
+      </button>
     </div>
   );
 }
