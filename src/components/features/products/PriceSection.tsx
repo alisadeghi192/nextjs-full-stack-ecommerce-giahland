fda80fa4c529dafc7b8e20f0ba7dcfd96a4527ed
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-17 19:22:40 +0330

    feat(products): add PriceSection component

---
+++ src/components/features/products/PriceSection.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { formatPrice } from "@/lib/utils/formatPrice";
+import { getDiscountedPrice } from "@/features/products/utils/productHelpers";
+
+interface PriceSectionProps {
+  price: number;
+  discount?: number;
+}
+
+export default function PriceSection({
+  price,
+  discount = 0,
+}: PriceSectionProps) {
+  const finalPrice = getDiscountedPrice(price, discount);
+  const hasDiscount = discount > 0;
+
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-x-2">
+      {hasDiscount && (
+        <span className="text-error relative mr-auto text-sm/6.25">
+          {formatPrice(price, false)}
+          <span className="bg-error mr-0-auto absolute top-1/2 block h-px w-full"></span>
+        </span>
+      )}
+      <span className="mr-auto text-lg/8 max-sm:text-base/7.25">
+        {formatPrice(finalPrice)}
+      </span>
+    </div>
+  );
+}
