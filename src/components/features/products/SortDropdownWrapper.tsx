8a816bba6eb036313aa697377d4112fffa82da0c
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-18 20:53:07 +0330

    refactor(products): remove deprecated components (replaced by shared/ui versions)

--- src/components/features/products/SortDropdownWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import SortDropdown from "./SortDropdown";
-import { useProductsParams } from "@/features/products/hooks/useProductsParams";
-
-export default function SortDropdownWrapper() {
-  const { selectedSort, setSort } = useProductsParams();
-  return <SortDropdown selectedSort={selectedSort} onSortChange={setSort} />;
-}
