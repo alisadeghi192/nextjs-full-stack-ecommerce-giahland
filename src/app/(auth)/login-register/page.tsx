6c936ecb2d2bcbfa56f34f106f14089ec6c884c4
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-18 12:23:49 +0330

    refactor(auth): make auth page server component with AuthToggle

--- src/app/(auth)/login-register/page.tsx
+++ src/app/(auth)/login-register/page.tsx
@@ -1,26 +1,14 @@
-"use client";
-
-import { useState } from "react";
-import LoginForm from "@/components/features/auth/LoginForm";
-import RegisterForm from "@/components/features/auth/RegisterForm";
 import Image from "next/image";
 import PublicHeader from "@/components/layout/public/PublicHeader";
-
-type authTypes = "login" | "register";
+import AuthToggle from "@/components/features/auth/AuthToggle";
 
 export default function AuthPage() {
-  const [authType, setAuthType] = useState<authTypes>("login");
-
   return (
     <main className="relative flex h-dvh items-center max-sm:flex-col-reverse">
       <div className="absolute top-0 right-0 left-0 sm:hidden">
         <PublicHeader />
       </div>
-      {authType === "register" ? (
-        <RegisterForm onToggle={() => setAuthType("login")} />
-      ) : (
-        <LoginForm onToggle={() => setAuthType("register")} />
-      )}
+      <AuthToggle />
       <div className="relative h-screen w-1/2 max-lg:w-1/3 max-sm:h-3/8 max-sm:w-full">
         <Image
           alt="login page pic"
