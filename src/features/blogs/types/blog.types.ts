48039592a4a1a05ec44552ebb98e69b17df9572c
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-21 15:32:12 +0330

    feat: update blog.types.ts with ContentBlock type for paragraph, image, heading, bulletList, orderedList

--- src/features/blogs/types/blog.types.ts
+++ src/features/blogs/types/blog.types.ts
@@ -1,3 +1,40 @@
+
+export type ContentBlock =
+  | {
+      type: "paragraph";
+      data: {
+        text: string;
+      };
+    }
+  | {
+      type: "image";
+      data: {
+        src: string;
+        alt: string;
+        caption?: string;
+      };
+    }
+  | {
+      type: "heading";
+      data: {
+        text: string;
+        level?: 1 | 2 | 3 | 4;
+      };
+    }
+  | {
+      type: "bulletList";
+      data: {
+        items: string[];
+      };
+    }
+  | {
+      type: "orderedList";
+      data: {
+        items: string[];
+      };
+    };
+
+
 export interface BlogPostType {
   id: number;
   title: string;
@@ -10,13 +47,5 @@ export interface BlogPostType {
   category: "care" | "health" | "intro";
   views: number;
   publishedAt: Date;
-  content?: {
-    type: string;
-    data: {
-      text?: string;
-      src?: string;
-      alt?: string;
-      caption?: string;
-    };
-  }[];
+  content?: ContentBlock[];
 }
