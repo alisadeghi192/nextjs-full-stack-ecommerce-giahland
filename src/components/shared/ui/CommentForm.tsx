6ac3e70c15fe71c651a10886e9b3f01064f712f4
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-23 17:33:31 +0330

    feat(blog): extract CommentForm component

---
+++ src/components/shared/ui/CommentForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import FormField from "@/components/shared/ui/FormField";
+import TextareaField from "@/components/shared/ui/TextareaField";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+
+export default function CommentForm() {
+  return (
+    <div className="border-neutral3 mb-4 flex flex-col space-y-4 rounded-xl border p-6">
+      <h3 className="font-bold">دیدگاه خود را ثبت کنید.</h3>
+      <form className="space-y-4">
+        <div className="flex items-center justify-between gap-x-4 gap-y-4 *:w-full max-[576px]:flex-col">
+          <FormField
+            icon={<MdDriveFileRenameOutline size={22} />}
+            type="text"
+            name="comment-name"
+            label="نام و نام خانوادگی"
+          />
+          <FormField
+            icon={<MdDriveFileRenameOutline size={22} />}
+            type="email"
+            name="comment-email"
+            label="ایمیل"
+          />
+        </div>
+        <TextareaField
+          icon={<MdDriveFileRenameOutline size={22} />}
+          name="comment-text"
+          label="متن دیدگاه"
+          rows={3}
+        />
+        <button
+          type="submit"
+          className="text-WHITE bg-primary max-xs:w-full hover:bg-shade2 flex h-10 w-47 cursor-pointer items-center justify-center justify-self-end rounded-lg transition-colors"
+        >
+          ارسال
+        </button>
+      </form>
+    </div>
+  );
+}
