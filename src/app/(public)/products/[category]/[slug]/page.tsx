a208625ae6462ddef2ef0ee1f46ea503d0ce1f3d
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-04-29 19:02:24 +0330

    feat(product): complete features section with detailed content and images

--- src/app/(public)/products/[category]/[slug]/page.tsx
+++ src/app/(public)/products/[category]/[slug]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { notFound } from "next/navigation";
 import Breadcrumb from "@/components/shared/ui/Breadcrumb";
 import { fakeProducts } from "@/data/products";
@@ -9,9 +8,10 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 import { MdOutlineChangeCircle } from "react-icons/md";
 import { BiSupport } from "react-icons/bi";
 import { AiOutlineDollarCircle } from "react-icons/ai";
-import PriceSection from "@/components/features/products/PriceSection";
 import { formatPrice } from "@/lib/utils/format";
 import PrimaryButton from "@/components/shared/ui/PrimaryButton";
+import ProductDetailTabs from "@/components/features/products/ProductDetailTabs";
+import { productDetailTabs } from "@/lib/constants";
 
 interface ProductPageProps {
   params: Promise<{
@@ -39,20 +39,19 @@ export default async function ProductPage({ params }: ProductPageProps) {
     <main className="container">
       <Breadcrumb title={product.name} />
 
-      <section className="flex items-end gap-x-8">
-        <div className="flex basis-220 items-start justify-center gap-x-6">
+      <section className="mb-10 flex items-end gap-x-8">
+        <div className="flex basis-220 items-end justify-center gap-x-6">
           {/* gallery */}
           <div className="flex max-h-124 basis-89 flex-col items-center justify-center gap-y-4">
             {/* big pic */}
-            <div className="relative cursor-pointer overflow-hidden rounded-lg">
+            <div className="relative aspect-square w-full cursor-pointer overflow-hidden rounded-lg">
               <Image
-                width={356}
-                height={366}
                 src={product.image}
-                alt="babaadam"
-                className="object-cover"
+                alt={product.name}
+                fill
+                className="object-contain"
                 priority
-              ></Image>
+              />
               <LikeButton className="top-4 right-4" />
             </div>
             {/* thumbnails */}
@@ -64,7 +63,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
                   src={product.image}
                   alt="babaadam"
                   className="aspect-square object-cover"
-                ></Image>
+                />
               </div>{" "}
               <div className="border-neutral6 rounded-lg border p-1">
                 <Image
@@ -73,7 +72,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
                   src={product.image}
                   alt="babaadam"
                   className="aspect-square object-cover"
-                ></Image>
+                />
               </div>{" "}
               <div className="border-neutral6 rounded-lg border p-1">
                 <Image
@@ -82,7 +81,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
                   src={product.image}
                   alt="babaadam"
                   className="aspect-square object-cover"
-                ></Image>
+                />
               </div>{" "}
               <div className="border-neutral6 rounded-lg border p-1">
                 <Image
@@ -91,7 +90,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
                   src={product.image}
                   alt="babaadam"
                   className="aspect-square object-cover"
-                ></Image>
+                />
               </div>
             </div>
           </div>
@@ -174,13 +173,118 @@ export default async function ProductPage({ params }: ProductPageProps) {
             <span className="text-xl/9">{formatPrice(product.price)} </span>
           </div>
 
-          <PrimaryButton
-            onClick={() => console.log("add to cart")}
-            className="text-lg/8 w-full h-12"
-          >
+          <PrimaryButton className="h-12 w-full text-lg/8">
             افزودن به سبد خرید
           </PrimaryButton>
         </div>
+      </section>
+      <section>
+          <ProductDetailTabs tabs={productDetailTabs} />
+        <div
+          id="features"
+          className="border-tint7 space-y-6 border-b border-dashed py-6 scroll-mt-25"
+        >
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <h5 className="leading-7.25 font-bold">
+                معرفی کلی گیاه {product.name}
+              </h5>
+              <p className="text-neutral10 text-justify leading-7.25">
+                یوکا سرده‌ای از گیاهان همیشه‌سبز و شبیه به درختچه یا درخت است که
+                بومی مناطق گرم و خشک آمریکای شمالی، آمریکای مرکزی و کارائیب
+                می‌باشد. این گیاه عضوی از خانواده مارچوبگان (Asparagaceae) و
+                زیرخانواده آگاووئیدها (Agavoideae) است.
+                <br />
+                نام علمی رایج: Yucca (گونه‌های مختلف مانند Yucca gigantea, Yucca
+                elephantipes, Yucca aloifolia)
+                <br />
+                نام‌های انگلیسی: Yucca, Spineless Yucca (یوکای بدون تیغ), Cane
+                Yucca (یوکای نیشکر)
+              </p>
+            </div>
+            <div className="space-y-2">
+              <h5 className="leading-7.25 font-bold">ویژگی های ظاهری</h5>
+              <div className="text-neutral10 text-justify leading-7.25">
+                <ul className="text-neutral10 max-xs:text-sm/6.25 list-disc pr-6 text-justify leading-7.25">
+                  <li>
+                    ساقه:دارای ساقه‌ای چوبی و ضخیم (شبیه به پای فیل) که در برخی
+                    گونه‌ها به ارتفاع چند متر می‌رسد.
+                  </li>
+                  <li>
+                    برگ‌ها:بلند، باریک، شمشیری شکل و نوک تیز (در برخی گونه‌ها
+                    بسیار تیز و خطرناک). برگ‌ها به صورت دسته‌ای از بالای ساقه
+                    بیرون می‌آیند.
+                  </li>
+                  <li>
+                    رنگ برگ:سبز تیره، سبز مایل به آبی، یا ابلق (سبز با حاشیه زرد
+                    یا سفید).
+                  </li>
+                  <li>
+                    گل: در طبیعت، یوکا گل‌های زنگوله‌ای شکل، سفید یا کرم رنگ به
+                    صورت خوشه‌ای عمودی و بسیار معطر تولید می‌کند. اما در شرایط
+                    آپارتمانی به ندرت گل می‌دهد.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <h5 className="leading-7.25 font-bold">نکات ویژه و هشدارها</h5>
+              <div className="text-neutral10 text-justify leading-7.25">
+                <ol className="text-neutral10 max-xs:text-sm/6.25 list-decimal pr-6 text-justify leading-7.25">
+                  <li>
+                    سمی بودن: یوکا برای گربه، سگ و اسب سمی است. ساپونین موجود در
+                    گیاه می‌تواند باعث استفراغ، اسهال و ترشح بزاق در حیوانات
+                    خانگی شود. دور از دسترس حیوانات نگهداری کنید.
+                  </li>
+                  <li>
+                    تیغ‌های خطرناک: در گونه‌هایی مانند Yucca aloifolia، نوک
+                    برگ‌ها بسیار تیز است و می‌تواند پوست را سوراخ کند. هنگام
+                    جابجایی دستکش بپوشید.
+                  </li>
+                  <li>
+                    تصفیه هوا: یوکا یکی از گیاهان مؤثر در جذب ترکیبات آلی فرار
+                    (مانند فرمالدئید، بنزن و تری کلرواتیلن) است. (طبق مطالعه
+                    ناسا)
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <h5 className="leading-7.25 font-bold">تکثیر {product.name}</h5>
+              <div className="text-neutral10 text-justify leading-7.25">
+                <ol className="text-neutral10 max-xs:text-sm/6.25 list-decimal pr-6 text-justify leading-7.25">
+                  <li>
+                    قلمه ساقه:برش ۱۰-۲۰ سانتی‌متری از ساقه، خشک کردن سطح برش به
+                    مدت ۲ روز، کاشت در ماسه مرطوب.
+                  </li>
+                  <li>پاجوش:جدا کردن پاجوش‌های اطراف گیاه مادری در بهار.</li>
+                  <li>بذر: زمان‌بر و تخصصی، نیاز به دمای ۲۰-۲۵ درجه.</li>
+                </ol>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <h5 className="leading-7.25 font-bold">جمع بندی</h5>
+              <p className="text-neutral10 text-justify leading-7.25">
+                یوکا گیاهی صبور، کم‌توقع و زیبا است که با ساقه ضخیم و تاجی از
+                برگ‌های شمشیری شکل، جلوهای مدرن و گرمسیری به خانه می‌بخشد. تنها
+                کلید موفقیت با آن، خشک نگه داشتن خاک بین دو آبیاری و نور کافی
+                است. اگر اهل آبیاری زیاد هستید، این گیاه برای شما مناسب نیست،
+                اما اگر به دنبال گیاهی مقاوم و ماندگار می‌گردید، یوکا انتخابی
+                عالی است.
+              </p>
+            </div>
+            <div className="border-neutral4 mx-auto mt-10 w-fit overflow-hidden rounded-2xl border">
+              <Image
+                alt="pro"
+                src={product.image}
+                width={360}
+                height={360}
+                className="object-contain"
+              />
+            </div>
+          </div>
+        </div>
+        <div id="cares"></div>
       </section>
     </main>
   );
