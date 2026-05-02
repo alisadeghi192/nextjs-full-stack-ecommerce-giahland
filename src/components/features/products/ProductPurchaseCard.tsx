5e6c1e48b8db21bfc957fc826984d2f90e87d3c7
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-02 12:13:25 +0330

    refactor(purchase-card): use shared PriceSection component

--- src/components/features/products/ProductPurchaseCard.tsx
+++ src/components/features/products/ProductPurchaseCard.tsx
@@ -4,19 +4,27 @@ import { BiSupport } from "react-icons/bi";
 import { AiOutlineDollarCircle } from "react-icons/ai";
 import { formatPrice } from "@/lib/utils/format";
 import PrimaryButton from "@/components/shared/ui/PrimaryButton";
+import DiscountBadge from "./DiscountBadge";
+import { getDiscountedPrice } from "@/features/products/utils/productHelpers";
+import PriceSection from "./PriceSection";
 
 interface ProductPurchaseCardProps {
   price: number;
+  discount: number;
   onAddToCart?: () => void;
 }
 
 export default function ProductPurchaseCard({
   price,
   onAddToCart,
+  discount,
 }: ProductPurchaseCardProps) {
+  const hasDiscount = discount > 0;
+  const finalPrice = getDiscountedPrice(price, discount);
+
   return (
     <div className="border-neutral7 max-xs:w-full w-78 rounded-2xl border px-6 py-7.75 max-xl:mt-9 max-sm:mt-8 max-sm:self-center">
-      <div className="space-y-2 border-b pb-6">
+      <div className="space-y-2">
         <div className="bg-neutral3 flex items-center gap-x-3 rounded-xl p-3">
           <MdOutlineChangeCircle className="text-shade1 size-7.5" />
           <span className="text-neutral9 text-sm/6 font-medium">
@@ -37,10 +45,7 @@ export default function ProductPurchaseCard({
         </div>
       </div>
 
-      <div className="my-6 flex items-center justify-between">
-        <span className="leading-7.25">قیمت:</span>
-        <span className="text-xl/9">{formatPrice(price)}</span>
-      </div>
+      <PriceSection price={price} discount={discount} variant="purchase-card"/>
 
       <PrimaryButton onClick={onAddToCart} className="h-12 w-full text-lg/8">
         افزودن به سبد خرید
