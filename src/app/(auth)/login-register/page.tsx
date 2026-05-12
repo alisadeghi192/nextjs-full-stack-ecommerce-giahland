f6127afc264bdfc6092c6f305d2fa8b95969d180
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-12 21:17:34 +0330

    style(auth): replace PublicHeader with MobileNav in login page

--- src/app/(auth)/login-register/page.tsx
+++ src/app/(auth)/login-register/page.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
-import PublicHeader from "@/components/shared/layout/public/header/PublicHeader";
 import AuthToggle from "@/components/features/auth/AuthToggle";
+import MobileNav from "@/components/shared/layout/public/header/MobileNav";
 
 export default function AuthPage() {
   return (
     <main className="relative flex h-dvh items-center max-sm:flex-col-reverse">
-      <div className="absolute top-0 right-0 left-0 sm:hidden">
-        <PublicHeader hasSearchInput={false} />
+      <div className="absolute top-0 right-0 left-0 z-20  sm:hidden ">
+        <MobileNav hasSearchInput={false} isScrolled={true} useInLoginPage={true}/>
       </div>
       <AuthToggle />
-      <div className="relative h-screen w-1/2 max-lg:w-1/3 max-sm:h-3/8 max-sm:w-full">
+      <div className="relative h-dvh w-1/2 max-lg:w-1/3 max-sm:h-3/8 max-sm:w-full ">
         <Image
           alt="login page pic"
           src="/static/images/login-cover.png"
           fill
-          className="object-cover max-sm:mt-10"
+          className="object-cover  max-sm:mt-12"
         />
       </div>
     </main>
