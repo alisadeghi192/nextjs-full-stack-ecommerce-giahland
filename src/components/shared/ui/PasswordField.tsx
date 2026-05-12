1457c714ead66f36c22ee660c453a227551dc7a6
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-12 21:10:59 +0330

    feat(ui): add form fields with error handling and react-hook-form support

--- src/components/shared/ui/PasswordField.tsx
+++ src/components/shared/ui/PasswordField.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 import {
   MdOutlineKey,
   MdOutlineVisibility,
@@ -9,51 +9,64 @@ import {
 
 interface PasswordFieldProps {
   name: string;
+  id: string;
   label: string;
+  error?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export default function PasswordField({
-  name,
-  label,
-  value,
-  onChange,
-}: PasswordFieldProps) {
-  const [showPassword, setShowPassword] = useState(false);
+const PasswordField = forwardRef<HTMLInputElement, PasswordFieldProps>(
+  ({ name, id, label, error, value, onChange, onBlur }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const hasError = !!error;
 
-  return (
-    <div className="border-neutral6 group focus-within:border-primary relative flex h-14 items-center gap-x-2 rounded-xl border px-3.75 transition-colors duration-200 max-sm:h-12">
-      <MdOutlineKey className="text-neutral9 group-focus-within:text-primary relative z-10 size-5.5 transition-colors duration-200" />
+    return (
+      <div >
+        <div
+          className={`group relative flex h-14  items-center gap-x-2 rounded-xl border px-3.75 transition-colors duration-200 max-sm:h-12 ${hasError ? "border-error focus-within:border-error" : "border-neutral6 focus-within:border-primary"} `}
+        >
+          <MdOutlineKey
+            className={`relative z-10 size-5.5 transition-colors duration-200 ${hasError ? "text-error group-focus-within:text-error" : "text-neutral9 group-focus-within:text-primary"}`}
+          />
 
-      <input
-        className="peer text-neutral11 flex-1 border-0 outline-0"
-        type={showPassword ? "text" : "password"}
-        name={name}
-        id={name}
-        placeholder=" "
-        value={value}
-        onChange={onChange}
-      />
+          <input
+            ref={ref}
+            className="peer text-neutral11 flex-1 border-0 outline-0"
+            type={showPassword ? "text" : "password"}
+            name={name}
+            id={id}
+            placeholder=" "
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
 
-      <label
-        htmlFor={name}
-        className="text-neutral9 peer-focus:text-primary peer-[:not(:placeholder-shown)]:text-primary absolute right-10.25 bg-white px-1 transition-all duration-300 peer-focus:right-4 peer-focus:-translate-y-7 peer-[:not(:placeholder-shown)]:right-4 peer-[:not(:placeholder-shown)]:-translate-y-7 max-sm:peer-focus:text-sm max-sm:peer-[:not(:placeholder-shown)]:text-sm"
-      >
-        {label}
-      </label>
+          <label
+            htmlFor={id}
+            className={`absolute right-10.25 rounded-2xl bg-white px-1 transition-all duration-300 peer-focus:right-4 peer-focus:-translate-y-7 peer-[:not(:placeholder-shown)]:right-4 peer-[:not(:placeholder-shown)]:-translate-y-7 max-sm:peer-focus:text-sm max-sm:peer-[:not(:placeholder-shown)]:text-sm ${hasError ? "text-error peer-focus:text-error peer-[:not(:placeholder-shown)]:text-error" : "text-neutral9 peer-focus:text-primary peer-[:not(:placeholder-shown)]:text-primary"}`}
+          >
+            {label}
+          </label>
 
-      <button
-        type="button"
-        onClick={() => setShowPassword(!showPassword)}
-        className="text-neutral9 group-focus-within:text-primary relative z-10 transition-colors duration-200"
-      >
-        {showPassword ? (
-          <MdOutlineVisibilityOff size={22} />
-        ) : (
-          <MdOutlineVisibility size={22} />
-        )}
-      </button>
-    </div>
-  );
-}
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className={`relative z-10 transition-colors cursor-pointer duration-200 ${hasError ? "text-error group-focus-within:text-error" : "text-neutral9 group-focus-within:text-primary"}`}
+            tabIndex={-1}
+          >
+            {showPassword ? (
+              <MdOutlineVisibilityOff size={22} />
+            ) : (
+              <MdOutlineVisibility size={22} />
+            )}
+          </button>
+        </div>
+        {error && <p className=" text-error text-sm">{error}</p>}
+      </div>
+    );
+  },
+);
+
+export default PasswordField;
