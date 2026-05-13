4e9ae4a4a0ec44b77d4568db3c94e90e7b964f2e
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-13 15:11:16 +0330

    refactor(auth): use LoginFormWrapper in AuthToggle

--- src/components/features/auth/AuthToggle.tsx
+++ src/components/features/auth/AuthToggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import LoginForm from "./LoginForm";
 import RegisterFormWrapper from "./RegisterFormWrapper";
+import LoginFormWrapper from "./LoginFormWrapper";
 
 export default function AuthToggle() {
   const [authType, setAuthType] = useState<"login" | "register">("login");
@@ -11,7 +11,7 @@ export default function AuthToggle() {
       {authType === "register" ? (
         <RegisterFormWrapper onToggle={() => setAuthType("login")} />
       ) : (
-        <LoginForm onToggle={() => setAuthType("register")} />
+        <LoginFormWrapper onToggle={() => setAuthType("register")} />
       )}
     </>
   );
