9c1c1b4e96cf0c0b581a5e6b1a65d7aeaf691798
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-17 14:24:35 +0330

    refactor(header): integrate SearchButton into header actions

--- src/components/shared/layout/public/header/HeaderActions.tsx
+++ src/components/shared/layout/public/header/HeaderActions.tsx
@@ -2,14 +2,15 @@
 
 import { useState } from "react";
 import IconButton from "../../../ui/IconButton";
-import {
-  MdOutlineDarkMode,
-  MdOutlineSearch,
-} from "react-icons/md";
+import { MdOutlineDarkMode, MdOutlineSearch } from "react-icons/md";
 import AuthButtons from "./AuthButtons";
 import CartButton from "./CartButton";
 import Overlay from "./Overlay";
-import { HEADER_DESKTOP_DEFAULT, HEADER_DESKTOP_SCROLLED } from "@/lib/constants";
+import {
+  HEADER_DESKTOP_DEFAULT,
+  HEADER_DESKTOP_SCROLLED,
+} from "@/lib/constants";
+import SearchButton from "./SearchButton";
 
 interface HeaderActionsProps {
   isScrolled: boolean;
@@ -17,23 +18,53 @@ interface HeaderActionsProps {
 
 export default function HeaderActions({ isScrolled }: HeaderActionsProps) {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const headerHeight = isScrolled ? HEADER_DESKTOP_SCROLLED : HEADER_DESKTOP_DEFAULT;
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
-  const toggleCart = () => setIsCartOpen((prev) => !prev);
+  const headerHeight = isScrolled
+    ? HEADER_DESKTOP_SCROLLED
+    : HEADER_DESKTOP_DEFAULT;
+
   const closeCart = () => setIsCartOpen(false);
+  const closeSearch = () => setIsSearchOpen(false);
+  const closeAll = () => {
+    closeCart();
+    closeSearch();
+  };
+
+  const toggleSearch = () => {
+    if (isCartOpen) {
+      closeCart();
+    }
+    setIsSearchOpen((prev) => !prev);
+  };
+  const toggleCart = () => {
+    if (isSearchOpen) {
+      closeSearch();
+    }
+    setIsCartOpen((prev) => !prev);
+  };
 
   return (
     <div className="relative flex gap-x-4 max-xl:gap-x-1">
       <IconButton icon={<MdOutlineDarkMode size={24} />} />
-      <IconButton icon={<MdOutlineSearch size={24} />} />
-      
-      <CartButton isOpen={isCartOpen} onToggle={toggleCart} onClose={closeCart} />
-      
+
+      <SearchButton
+        isOpen={isSearchOpen}
+        onToggle={toggleSearch}
+        onClose={closeSearch}
+      />
+
+      <CartButton
+        isOpen={isCartOpen}
+        onToggle={toggleCart}
+        onClose={closeCart}
+      />
+
       <AuthButtons />
 
       <Overlay
-        isOpen={isCartOpen}
-        onClose={closeCart}
+        isOpen={isCartOpen || isSearchOpen}
+        onClose={closeAll}
         topOffset={headerHeight}
         zIndex={30}
       />
