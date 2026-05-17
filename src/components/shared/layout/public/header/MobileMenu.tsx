4e99344d72be19afc170c0cc8eb33189ffff618a
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-17 20:40:52 +0330

    style(mobile-menu): update login/logout buttons with new icons and styling

--- src/components/shared/layout/public/header/MobileMenu.tsx
+++ src/components/shared/layout/public/header/MobileMenu.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import React from "react";
-import { MdOutlineMenu } from "react-icons/md";
+import { MdOutlineLogin, MdOutlineLogout, MdOutlineMenu } from "react-icons/md";
 import { navLinks } from "@/lib/constants";
 import Logo from "./Logo";
 import { IoClose } from "react-icons/io5";
 import MobileNavLink from "./MobileNavLink";
 import Link from "next/link";
-import { TbLogin2, TbLogout2 } from "react-icons/tb";
 import {
   useIsAuthenticated,
   useAuthActions,
@@ -66,22 +65,22 @@ const MobileMenu = ({ isOpen, onClose, toggleMenu }: MobileMenuProps) => {
           <div className="bg-neutral2 mt-auto w-full rounded-lg">
             {isAuthenticated ? (
               <button
-                className="text-error flex h-14 w-full items-center justify-between px-4"
+                className="text-error flex h-14 w-full items-center gap-x-4 px-4"
                 onClick={handleLogout}
               >
+                <MdOutlineLogout className="size-6" />
                 <span className="leading-7.25 font-medium">
                   خروج از حساب کاربری
                 </span>
-                <TbLogout2 className="size-6" />
               </button>
             ) : (
               <Link
-                className="text-primary flex h-14 items-center justify-between px-4"
+                className="text-primary flex h-14 items-center gap-x-4 px-4"
                 href="/login-register"
                 onClick={onClose}
-              >
+                >
+                <MdOutlineLogin className="size-6" />
                 <span className="leading-7.25 font-medium">ورود/ثبت نام</span>
-                <TbLogin2 className="size-6" />
               </Link>
             )}
           </div>
