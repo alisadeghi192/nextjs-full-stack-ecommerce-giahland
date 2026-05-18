c91bcde75986f6a050c00d5e8be2ffe72d4e552e
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-18 19:14:01 +0330

    refactor(ui-selectors): remove unused single action selectors

--- src/stores/selectors/ui.selectors.ts
+++ src/stores/selectors/ui.selectors.ts
@@ -7,17 +7,6 @@ export const useIsMenuOpen = () => useUIStore((state) => state.isMenuOpen);
 export const useActiveNavHover = () => useUIStore((state) => state.activeNavHover);
 export const useOpenSubmenu = () => useUIStore((state) => state.openSubmenu);
 
-export const useOpenCart = () => useUIStore((state) => state.openCart);
-export const useCloseCart = () => useUIStore((state) => state.closeCart);
-export const useToggleCart = () => useUIStore((state) => state.toggleCart);
-
-export const useOpenSearch = () => useUIStore((state) => state.openSearch);
-export const useCloseSearch = () => useUIStore((state) => state.closeSearch);
-export const useToggleSearch = () => useUIStore((state) => state.toggleSearch);
-
-export const useOpenMenu = () => useUIStore((state) => state.openMenu);
-export const useCloseMenu = () => useUIStore((state) => state.closeMenu);
-export const useToggleMenu = () => useUIStore((state) => state.toggleMenu);
 
 export const useSetActiveNavHover = () => useUIStore((state) => state.setActiveNavHover);
 export const useSetOpenSubmenu = () => useUIStore((state) => state.setOpenSubmenu);
