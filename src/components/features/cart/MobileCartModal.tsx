f816b5914b1737b9417d4922837b616c4c313b54
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-18 19:12:06 +0330

    refactor(mobile-cart-modal): remove props, use UI store directly

--- src/components/features/cart/MobileCartModal.tsx
+++ src/components/features/cart/MobileCartModal.tsx
@@ -1,23 +1,21 @@
 "use client";
 
 import CartModal from "./CartModal";
+import { useIsCartOpen, useCartActions } from "@/stores/selectors/ui.selectors";
 
-interface MobileCartModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
-
-export default function MobileCartModal({ isOpen, onClose }: MobileCartModalProps) {
+export default function MobileCartModal() {
+  const isCartOpen = useIsCartOpen();
+  const { closeCart } = useCartActions();
   return (
     <div className="relative">
       <div
         className={`absolute -top-10 left-0 z-40 w-full transition-all duration-200 ${
-          isOpen
+          isCartOpen
             ? "visible translate-y-0 opacity-100"
             : "invisible translate-y-2 opacity-0"
         }`}
       >
-        <CartModal onClose={onClose} />
+        <CartModal onClose={closeCart} />
       </div>
     </div>
   );
