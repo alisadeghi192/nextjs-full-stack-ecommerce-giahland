03b052b05da2dbb43af5d41298a1320708540bdd
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-18 20:23:21 +0330

    feat(cart-button): add Persian badge with temporary static value

--- src/components/shared/layout/public/header/CartButton.tsx
+++ src/components/shared/layout/public/header/CartButton.tsx
@@ -4,13 +4,26 @@ import IconButton from "@/components/shared/ui/IconButton";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import CartModal from "@/components/features/cart/CartModal";
 import { useIsCartOpen, useCartActions } from "@/stores/selectors/ui.selectors";
+import { toPersianNumber } from "@/lib/utils/format";
+
+const totalItems = 2;
 
 export default function CartButton() {
   const isCartOpen = useIsCartOpen();
   const { toggleCart, closeCart } = useCartActions();
   return (
-    <div className="relative" >
-      <IconButton icon={<MdOutlineShoppingCart size={24} />} onClick={toggleCart}/>
+    <div className="relative">
+      <div className="relative">
+        <IconButton
+          icon={<MdOutlineShoppingCart size={24} />}
+          onClick={toggleCart}
+        />
+        {totalItems > 0 && (
+          <span className="bg-error absolute font-medium text-white text-[10px] -top-1.25 -right-1.25 flex size-5.5 items-center justify-center rounded-full">
+            {toPersianNumber(totalItems)}
+          </span>
+        )}
+      </div>
 
       <div
         className={`absolute top-13.5 left-0 z-70 transition-all duration-200 max-lg:top-12.5 ${
