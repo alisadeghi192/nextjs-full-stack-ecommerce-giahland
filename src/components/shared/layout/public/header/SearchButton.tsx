1f23539fa9f7503867c239ae6f3d16db4bf08f85
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-18 17:21:59 +0330

    refactor(search-button): integrate UI store, remove props

--- src/components/shared/layout/public/header/SearchButton.tsx
+++ src/components/shared/layout/public/header/SearchButton.tsx
@@ -3,30 +3,24 @@
 import IconButton from "@/components/shared/ui/IconButton";
 import { MdOutlineSearch } from "react-icons/md";
 import SearchModalContent from "./SearchModalContent";
+import { useIsSearchOpen, useSearchActions } from "@/stores/selectors/ui.selectors";
 
-interface SearchButtonProps {
-  isOpen: boolean;
-  onToggle: () => void;
-  onClose: () => void;
-}
 
-export default function SearchButton({
-  isOpen,
-  onToggle,
-  onClose,
-}: SearchButtonProps) {
+export default function SearchButton() {
+  const isSearchOpen = useIsSearchOpen();
+  const { toggleSearch, closeSearch } = useSearchActions();
   return (
     <div className="relative">
-      <IconButton icon={<MdOutlineSearch size={24} />} onClick={onToggle} />
+      <IconButton icon={<MdOutlineSearch size={24} />} onClick={toggleSearch} />
 
       <div
         className={`absolute top-13.5 left-0 z-70 shadow-lg transition-all duration-200 max-lg:top-12.5 ${
-          isOpen
+          isSearchOpen
             ? "visible translate-y-0 opacity-100"
             : "invisible translate-y-2 opacity-0"
         }`}
       >
-        <SearchModalContent   onClose={onClose} isOpen={isOpen} />
+        <SearchModalContent   onClose={closeSearch} isOpen={isSearchOpen} />
       </div>
     </div>
   );
