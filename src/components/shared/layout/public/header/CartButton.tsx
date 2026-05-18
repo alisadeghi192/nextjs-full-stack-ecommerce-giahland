557ed01d818d56611145aa0eb171cfc1529636fd
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-05-18 17:23:08 +0330

    refactor(cart-button): integrate UI store, remove props

--- src/components/shared/layout/public/header/CartButton.tsx
+++ src/components/shared/layout/public/header/CartButton.tsx
@@ -3,26 +3,23 @@
 import IconButton from "@/components/shared/ui/IconButton";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import CartModal from "@/components/features/cart/CartModal";
+import { useIsCartOpen, useCartActions } from "@/stores/selectors/ui.selectors";
 
-interface CartButtonProps {
-  isOpen: boolean;
-  onToggle: () => void;
-  onClose: () => void;
-}
-
-export default function CartButton({ isOpen, onToggle, onClose }: CartButtonProps) {
+export default function CartButton() {
+  const isCartOpen = useIsCartOpen();
+  const { toggleCart, closeCart } = useCartActions();
   return (
-    <div className="relative" onClick={onToggle}>
-      <IconButton icon={<MdOutlineShoppingCart size={24} />} />
+    <div className="relative" >
+      <IconButton icon={<MdOutlineShoppingCart size={24} />} onClick={toggleCart}/>
 
       <div
-        className={`absolute  top-13.5 max-lg:top-12.5 left-0 z-70 transition-all duration-200 ${
-          isOpen
+        className={`absolute top-13.5 left-0 z-70 transition-all duration-200 max-lg:top-12.5 ${
+          isCartOpen
             ? "visible translate-y-0 opacity-100"
             : "invisible translate-y-2 opacity-0"
         }`}
       >
-        <CartModal onClose={onClose} />
+        <CartModal onClose={closeCart} />
       </div>
     </div>
   );
