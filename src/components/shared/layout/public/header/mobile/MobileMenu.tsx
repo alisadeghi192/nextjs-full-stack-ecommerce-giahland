f5575214623f8ced47c4fcb200915c514262d483
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-06-06 17:27:20 +0330

    feat(mobile-menu): add confirm dialog for logout action

--- src/components/shared/layout/public/header/mobile/MobileMenu.tsx
+++ src/components/shared/layout/public/header/mobile/MobileMenu.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { MdOutlineLogin, MdOutlineLogout, MdOutlineMenu } from "react-icons/md";
 import { navLinks } from "@/lib/constants";
-import {Logo , MobileNavLink } from "..";
+import { Logo, MobileNavLink } from "..";
 import { IoClose } from "react-icons/io5";
 import Link from "next/link";
 import {
@@ -12,8 +12,7 @@ import {
 } from "@/features/auth/selectors/auth.selectors";
 import { useIsMenuOpen, useMenuActions } from "@/stores/selectors/ui.selectors";
 import { HiOutlineLogout } from "react-icons/hi";
-
-
+import ConfirmDialog from "@/components/shared/ui/ConfirmDialog";
 
 const MobileMenu = () => {
   const isAuthenticated = useIsAuthenticated();
@@ -35,14 +34,13 @@ const MobileMenu = () => {
         <MdOutlineMenu className="size-5 sm:size-6" />
       </button>
       <div
-        className={`border-neutral3 fixed top-0 -right-82 z-40 h-dvh w-82 rounded-tl-xl  rounded-bl-xl border bg-white p-4 shadow-lg transition-all duration-200 ${
+        className={`border-neutral3 fixed top-0 -right-82 z-40 h-dvh w-82 rounded-tl-xl rounded-bl-xl border bg-white p-4 shadow-lg transition-all duration-200 ${
           isMenuOpen
             ? "visible -translate-x-82 opacity-100"
             : "invisible translate-x-82 opacity-0"
         }`}
       >
         <div className="flex h-full flex-col">
-
           <div className="mb-4 flex items-center justify-between">
             <Logo />
             <button onClick={closeMenu}>
@@ -50,7 +48,7 @@ const MobileMenu = () => {
             </button>
           </div>
 
-          <div className="divide-neutral3 flex flex-col divide-y overflow-y-auto overflow-x-hidden">
+          <div className="divide-neutral3 flex flex-col divide-y overflow-x-hidden overflow-y-auto">
             {navLinks.map((link) => (
               <MobileNavLink
                 key={link.href}
@@ -65,25 +63,26 @@ const MobileMenu = () => {
             ))}
           </div>
 
-
-
           <div className="bg-neutral2 mt-auto w-full rounded-lg">
             {isAuthenticated ? (
-              <button
+              <ConfirmDialog
+                onConfirm={handleLogout}
+                title="آیا مایل به خروج از حساب کاربری هستید؟"
+                confirmText="بله، خروج"
+                cancelText="خیر"
                 className="text-error flex h-14 w-full items-center gap-x-4 px-4"
-                onClick={handleLogout}
               >
-                <HiOutlineLogout  className="size-6" />
+                <HiOutlineLogout className="size-6" />
                 <span className="leading-7.25 font-medium">
                   خروج از حساب کاربری
                 </span>
-              </button>
+              </ConfirmDialog>
             ) : (
               <Link
                 className="text-primary flex h-14 items-center gap-x-4 px-4"
                 href="/login-register"
                 onClick={closeMenu}
-                >
+              >
                 <MdOutlineLogin className="size-6" />
                 <span className="leading-7.25 font-medium">ورود/ثبت نام</span>
               </Link>
