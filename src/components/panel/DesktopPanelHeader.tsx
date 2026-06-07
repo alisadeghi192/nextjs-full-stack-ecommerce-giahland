3d2915b6fce53436c354ff1fa211dfb914f2ee47
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-06-07 19:53:37 +0330

    refactor(panel): extract desktop and mobile headers without changing logic

---
+++ src/components/panel/DesktopPanelHeader.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { Logo } from "@/components/shared/layout/public/header";
+import IconButton from "@/components/shared/ui/IconButton";
+import { MdOutlineDarkMode } from "react-icons/md";
+import Image from "next/image";
+import {
+  useUserFirstName,
+  useUserAvatar,
+} from "@/features/auth/selectors/auth.selectors";
+import {
+  TbLayoutSidebarLeftExpandFilled,
+  TbLayoutSidebarRightExpandFilled,
+} from "react-icons/tb";
+
+interface DesktopPanelHeaderProps {
+  isScrolled: boolean;
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+export default function DesktopPanelHeader({
+  isScrolled,
+  isSidebarOpen,
+  toggleSidebar,
+}: DesktopPanelHeaderProps) {
+  const userFirstName = useUserFirstName();
+  const userAvatar = useUserAvatar();
+
+  return (
+    <nav className="border-neutral5 container border-b font-medium max-md:hidden">
+      <div
+        className={`text-neutral12 flex items-center justify-between bg-white transition-all duration-200 ${
+          isScrolled ? "h-15" : "h-24"
+        }`}
+      >
+        <div className="flex items-center gap-x-4">
+          <div
+            className="text-primary hover:text-shade2 cursor-pointer"
+            onClick={toggleSidebar}
+          >
+            {isSidebarOpen ? (
+              <TbLayoutSidebarLeftExpandFilled className="size-10" />
+            ) : (
+              <TbLayoutSidebarRightExpandFilled className="size-10" />
+            )}
+          </div>
+          <Logo />
+        </div>
+        <div className="flex items-center gap-x-4">
+          <IconButton icon={<MdOutlineDarkMode size={24} />} />
+          <button className="flex items-center gap-2">
+            <Image
+              src={userAvatar || "/static/images/default-user.jpg"}
+              alt="user"
+              width={48}
+              height={48}
+              className="size-12 rounded-full object-cover object-center max-lg:size-10"
+            />
+            <span className="text-lg max-lg:hidden">سلام {userFirstName}</span>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
