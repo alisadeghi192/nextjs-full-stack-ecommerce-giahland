f492cbdfb28c04711014bd58060f02650865b5e4
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-06-07 19:57:24 +0330

    refactor(products): change key from id to _id in ProductsList

--- src/components/features/products/ProductsList.tsx
+++ src/components/features/products/ProductsList.tsx
@@ -9,7 +9,7 @@ export default function ProductsList({ products }: ProductsListProps) {
   return (
     <div className="grid grid-cols-3 gap-6 max-xl:grid-cols-2 max-sm:grid-cols-1">
       {products.map((product) => (
-        <ProductCardList key={product.id} {...product} />
+        <ProductCardList key={product._id} {...product} />
       ))}
     </div>
   );
