75f9660c6a1621fdd79fa15761485e249e08978a
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-06-11 21:42:19 +0330

    perf(products-grid): fetch like statuses in bulk for all products

--- src/components/features/products/ProductsGrid.tsx
+++ src/components/features/products/ProductsGrid.tsx
@@ -1,15 +1,22 @@
+import { ProductCardData } from "@/features/products/types/product.types";
+import { getBulkLikeStatus } from "@/features/user/actions/wishlist.actions";
 import ProductCardGrid from "./ProductCardGrid";
-import {  ProductCardData } from "@/features/products/types/product.types";
 
 interface ProductsGridProps {
   products: ProductCardData[];
 }
 
-export default function ProductsGrid({ products }: ProductsGridProps) {
+export default async function ProductsGrid({ products }: ProductsGridProps) {
+  const productIds = products.map((product) => product._id);
+  const likeStatuses = await getBulkLikeStatus(productIds);
   return (
     <div className="grid grid-cols-4 gap-6 max-[400px]:grid-cols-1! max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-md:gap-4">
       {products.map((product) => (
-        <ProductCardGrid key={product._id} {...product} />
+        <ProductCardGrid
+          key={product._id}
+          {...product}
+          isLiked={likeStatuses[product._id]}
+        />
       ))}
     </div>
   );
