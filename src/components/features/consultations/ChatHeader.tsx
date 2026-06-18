fa5ff0da016f561b711a64f44c2c9bc26b827e1b
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-06-18 20:33:52 +0330

    feat(chat): add close consultation with redirect and toast delay

--- src/components/features/consultations/ChatHeader.tsx
+++ src/components/features/consultations/ChatHeader.tsx
@@ -4,6 +4,7 @@ import { closeConsultation } from "@/features/consultations/actions/closeConsult
 import { useIsSidebarOpen } from "@/stores/selectors/ui.selectors";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { MdArrowBack, MdStopCircle } from "react-icons/md";
@@ -28,13 +29,19 @@ export default function ChatHeader({
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const isChatActive = status === "active";
   const showFinishButton = isChatActive && isDoctor;
-  const isSidebarOpen = useIsSidebarOpen()
+  const isSidebarOpen = useIsSidebarOpen();
+
+  const router = useRouter();
 
   const finishChat = async () => {
     setIsLoading(true);
     const result = await closeConsultation(consultationId);
     if (result.success) {
       toast.success(result.message);
+      setTimeout(() => {
+        router.push("/user/consultations/list");
+      }, 1500); 
+      router.push("/user/consultations/list");
     } else {
       toast.error(result.message);
     }
@@ -42,7 +49,7 @@ export default function ChatHeader({
   };
 
   return (
-    <div className="sticky top-18 z-10 h-14.5 max-xs:h-12 mx-auto flex w-9/10 items-center gap-2 max-xs:gap-1 transition-all duration-200">
+    <div className="max-xs:h-12 max-xs:gap-1 sticky top-18 z-10 mx-auto flex h-14.5 w-9/10 items-center gap-2 transition-all duration-200">
       {/* close consultation button */}
       {showFinishButton && (
         <ConfirmDialog
@@ -51,19 +58,23 @@ export default function ChatHeader({
           confirmText="بله"
           title="آیا از بستن مشاوره مطمئنید؟"
           disabled={isLoading}
-          className={`border-neutral9 shrink-0 flex h-14 cursor-pointer items-center justify-center gap-x-2 rounded-full border bg-red-500 px-2 text-white transition-colors hover:bg-red-600 max-sm:size-14 max-xs:size-10 ${isSidebarOpen ? "max-lg:size-14" : ""}`}
+          className={`border-neutral9 max-xs:size-10 flex h-14 shrink-0 cursor-pointer items-center justify-center gap-x-2 rounded-full border bg-red-500 px-2 text-white transition-colors hover:bg-red-600 max-sm:size-14 ${isSidebarOpen ? "max-lg:size-14" : ""}`}
         >
-          <span className={`leading-tight max-sm:hidden ${isSidebarOpen ? "max-lg:hidden" : ""}`}>
+          <span
+            className={`leading-tight max-sm:hidden ${isSidebarOpen ? "max-lg:hidden" : ""}`}
+          >
             {isLoading ? "درحال بستن..." : "پایان مشاوره"}
           </span>
-          <MdStopCircle className={`size-8 shrink-0 ${isLoading ? "hidden" : ""}`} />
+          <MdStopCircle
+            className={`size-8 shrink-0 ${isLoading ? "hidden" : ""}`}
+          />
         </ConfirmDialog>
       )}
 
       {/* display person details */}
-      <div className="border-neutral5 flex w-full items-center justify-between rounded-full border bg-white p-1 max-xs:p-0 maxsm:h-10">
-        <div className="flex items-center gap-4 max-xs:gap-2 max-xs:w-full">
-          <div className="relative size-12 max-xs:size-10 overflow-hidden shrink-0 rounded-full">
+      <div className="border-neutral5 max-xs:p-0 maxsm:h-10 flex w-full items-center justify-between rounded-full border bg-white p-1">
+        <div className="max-xs:gap-2 max-xs:w-full flex items-center gap-4">
+          <div className="max-xs:size-10 relative size-12 shrink-0 overflow-hidden rounded-full">
             <Image
               src={displayAvatar}
               alt={displayName}
@@ -73,12 +84,14 @@ export default function ChatHeader({
           </div>
           <div className="max-xs:flex max-xs:justify-between max-xs:items-center max-xs:w-full">
             <h2 className="font-medium">{displayName}</h2>
-            <p className="text-primary text-sm max-xs:ml-2">{displayPersonRole}</p>
+            <p className="text-primary max-xs:ml-2 text-sm">
+              {displayPersonRole}
+            </p>
           </div>
         </div>
         {/* consultation status */}
         <div
-          className={`ml-2 rounded-full px-2 py-2 text-sm max-xs:hidden ${isChatActive ? "text-primary bg-green-100" : "bg-bg-error text-error"}`}
+          className={`max-xs:hidden ml-2 rounded-full px-2 py-2 text-sm ${isChatActive ? "text-primary bg-green-100" : "bg-bg-error text-error"}`}
         >
           <span>{isChatActive ? "چت فعال" : "چت بسته"}</span>
         </div>
@@ -86,9 +99,13 @@ export default function ChatHeader({
       {/* back button */}
       <Link
         href="/user/consultations/list"
-        className="border-neutral5 flex h-14 items-center justify-center gap-x-2 rounded-full border bg-gray-100 px-4 text-gray-600 transition-colors hover:bg-gray-200 max-xs:size-10"
+        className="border-neutral5 max-xs:size-10 flex h-14 items-center justify-center gap-x-2 rounded-full border bg-gray-100 px-4 text-gray-600 transition-colors hover:bg-gray-200"
       >
-        <span className={`max-sm:hidden ${isSidebarOpen ? "max-lg:hidden" : ""}`}>بازگشت</span>
+        <span
+          className={`max-sm:hidden ${isSidebarOpen ? "max-lg:hidden" : ""}`}
+        >
+          بازگشت
+        </span>
         <MdArrowBack className="size-6 shrink-0" />
       </Link>
     </div>
