a4b6f06632147bd5198a6ec0314d1368132e7ff0
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-06-28 13:20:11 +0330

    feat(article): add new article page with role protection

--- src/app/(panel)/user/(doctor)/articles/new/page.tsx
+++ src/app/(panel)/user/(doctor)/articles/new/page.tsx
@@ -1,6 +1,18 @@
+import ArticleForm from "@/components/features/blog/ArticleForm";
+import SectionTitle from "@/components/panel/SectionTitle";
+import { getMeAction } from "@/features/auth/actions/me.actions";
+import { redirect } from "next/navigation";
+export default async function NewArticlePage() {
+  const { user } = await getMeAction();
 
-export default function page() {
+  if (!user || (user.role !== "plant-doctor" && user.role !== "admin")) {
+    redirect("/user/profile");
+  }
+
   return (
-    <div>page</div>
-  )
+    <div className="w-full">
+      <SectionTitle title="نوشتن مقاله جدید" />
+      <ArticleForm />
+    </div>
+  );
 }
