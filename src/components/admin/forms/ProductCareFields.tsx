e26fba695e2a5ca787b45b6224fdad8c524ea7e6
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-07-10 21:35:28 +0330

    feat: add dynamic features and fixed cares components

---
+++ src/components/admin/forms/ProductCareFields.tsx
@@ -0,0 +1,187 @@
+"use client";
+
+import FormField from "@/components/shared/ui/FormField";
+import { ProductFormData } from "@/features/products/schemas/product.schema";
+import { useIsSidebarOpen } from "@/stores/selectors/ui.selectors";
+import { UseFormRegister } from "react-hook-form";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+
+interface ProductCareFieldsProps {
+  register: UseFormRegister<ProductFormData>;
+}
+
+export default function ProductCareFields({
+  register,
+}: ProductCareFieldsProps) {
+  // titles
+  const lightTitles = ["نیاز نوری", "مکان مناسب", "نکته مهم"];
+  const wateringTitles = ["قانون طلایی", "برنامه منظم", "هشدار مهم"];
+  const soilTitles = ["ترکیب عالی", "زهکشی حیاتی", "تعویض گلدان"];
+  const temperatureTitles = ["دمای ایده‌آل", "رطوبت مورد نیاز", "نکته فصلی"];
+  const fertilizationTitles = ["زمان کوددهی", "نوع و غلظت", "بسامد مناسب"];
+
+  const isSidebarOpen = useIsSidebarOpen();
+  return (
+    <div className="space-y-6">
+      {/* light */}
+
+      <div className="border-neutral4 rounded-xl border p-4 shadow-lg">
+        <h4 className="mb-4 font-medium">نور</h4>
+        {lightTitles.map((title, index) => (
+          <div
+            key={index}
+            className="mb-4 flex items-center gap-x-1.5 gap-y-2 max-lg:flex-col"
+          >
+            <div
+              className={`max-lg:self-start ${isSidebarOpen ? "w-1/5 max-xl:w-1/3 max-lg:w-2/3" : "w-1/7 max-xl:w-1/5 max-lg:w-1/3  max-sm:w-2/3"}`}
+            >
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`light-title-${index}`}
+                label={"عنوان"}
+                type="text"
+                defaultValue={title}
+                disabled={true}
+                {...register(`cares.light.${index}.title`)}
+              />
+            </div>
+            <span className="max-lg:hidden">:</span>
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`light-desc-${index}`}
+                label={"توضیحات"}
+                type="text"
+                {...register(`cares.light.${index}.description`)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* watering */}
+
+      <div className="border-neutral4 rounded-xl border p-4 shadow-lg">
+        <h4 className="mb-4 font-medium">آبیاری</h4>
+        {wateringTitles.map((title, index) => (
+          <div key={index} className="mb-4 flex items-center gap-x-1.5 gap-y-2 max-lg:flex-col">
+            <div className={`max-lg:self-start ${isSidebarOpen ? "w-1/5 max-xl:w-1/3 max-lg:w-2/3" : "w-1/7 max-xl:w-1/5 max-lg:w-1/3  max-sm:w-2/3"}`}>
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`watering-title-${index}`}
+                label={"عنوان"}
+                type="text"
+                defaultValue={title}
+                disabled={true}
+                {...register(`cares.watering.${index}.title`)}
+              />
+            </div>
+            <span className="max-lg:hidden">:</span>
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`watering-desc-${index}`}
+                label={"توضیحات"}
+                type="text"
+                {...register(`cares.watering.${index}.description`)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* soil */}
+      <div className="border-neutral4 rounded-xl border p-4 shadow-lg">
+        <h4 className="mb-4 font-medium">خاک</h4>
+        {soilTitles.map((title, index) => (
+          <div key={index} className="mb-4 flex items-center gap-x-1.5 gap-y-2 max-lg:flex-col">
+            <div className={`max-lg:self-start ${isSidebarOpen ? "w-1/5 max-xl:w-1/3 max-lg:w-2/3" : "w-1/7 max-xl:w-1/5 max-lg:w-1/3  max-sm:w-2/3"}`}>
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`soil-title-${index}`}
+                label={"عنوان"}
+                type="text"
+                defaultValue={title}
+                disabled={true}
+                {...register(`cares.soil.${index}.title`)}
+              />
+            </div>
+            <span className="max-lg:hidden">:</span>
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`soil-desc-${index}`}
+                label={"توضیحات"}
+                type="text"
+                {...register(`cares.soil.${index}.description`)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* temperature */}
+      <div className="border-neutral4 rounded-xl border p-4 shadow-lg">
+        <h4 className="mb-4 font-medium">دما و رطوبت</h4>
+        {temperatureTitles.map((title, index) => (
+          <div key={index} className="mb-4 flex items-center gap-x-1.5 gap-y-2 max-lg:flex-col">
+            <div className={`max-lg:self-start ${isSidebarOpen ? "w-1/5 max-xl:w-1/3 max-lg:w-2/3" : "w-1/7 max-xl:w-1/5 max-lg:w-1/3  max-sm:w-2/3"}`}>
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`temperature-title-${index}`}
+                label={"عنوان"}
+                type="text"
+                defaultValue={title}
+                disabled={true}
+                {...register(`cares.temperature.${index}.title`)}
+              />
+            </div>
+            <span className="max-lg:hidden">:</span>
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`temperature-desc-${index}`}
+                label={"توضیحات"}
+                type="text"
+                {...register(`cares.temperature.${index}.description`)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* fertilization */}
+      <div className="border-neutral4 rounded-xl border p-4 shadow-lg">
+        <h4 className="mb-4 font-medium">کوددهی</h4>
+        {fertilizationTitles.map((title, index) => (
+          <div
+            key={index}
+            className="mb-4 flex items-center gap-x-1.5 gap-y-2 max-lg:flex-col"
+          >
+            <div className={`max-lg:self-start ${isSidebarOpen ? "w-1/5 max-xl:w-1/3 max-lg:w-2/3" : "w-1/7 max-xl:w-1/5 max-lg:w-1/3  max-sm:w-2/3"}`}>
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`fertilization-title-${index}`}
+                label={`عنوان `}
+                type="text"
+                defaultValue={title}
+                disabled={true}
+                {...register(`cares.fertilization.${index}.title`)}
+              />
+            </div>
+            <span className="max-lg:hidden">:</span>
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`fertilization-desc-${index}`}
+                label={"توضیحات"}
+                type="text"
+                {...register(`cares.fertilization.${index}.description`)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/admin/forms/ProductFeaturesFields.tsx
@@ -0,0 +1,287 @@
+"use client";
+
+import FormField from "@/components/shared/ui/FormField";
+import { ProductFormData } from "@/features/products/schemas/product.schema";
+import { toPersianNumber } from "@/lib/utils/format";
+import { Control, useFieldArray } from "react-hook-form";
+import { MdAdd, MdDelete, MdDriveFileRenameOutline } from "react-icons/md";
+
+interface ProductFeaturesFieldsProps {
+  control: Control<ProductFormData>;
+  register: any;
+}
+
+export default function ProductFeaturesFields({
+  control,
+  register,
+}: ProductFeaturesFieldsProps) {
+  // ====== overview ======
+  const {
+    fields: overviewFields,
+    append: appendOverview,
+    remove: removeOverview,
+  } = useFieldArray({
+    control,
+    name: "features.overview",
+  });
+
+  // ====== appearance ======
+  const {
+    fields: appearanceFields,
+    append: appendAppearance,
+    remove: removeAppearance,
+  } = useFieldArray({
+    control,
+    name: "features.appearance",
+  });
+
+  // ====== warnings ======
+  const {
+    fields: warningsFields,
+    append: appendWarnings,
+    remove: removeWarnings,
+  } = useFieldArray({
+    control,
+    name: "features.warnings",
+  });
+
+  // ====== propagation ======
+  const {
+    fields: propagationFields,
+    append: appendPropagation,
+    remove: removePropagation,
+  } = useFieldArray({
+    control,
+    name: "features.propagation",
+  });
+
+  // ====== summary ======
+  const {
+    fields: summaryFields,
+    append: appendSummary,
+    remove: removeSummary,
+  } = useFieldArray({
+    control,
+    name: "features.summary",
+  });
+
+  return (
+    <div className="space-y-6">
+      {/* ====== Overview ====== */}
+      <div className="border-neutral4 rounded-xl border px-4 py-2 shadow-lg">
+        <div className="flex items-center justify-between">
+          <div>
+            <h4 className="mb-2 font-medium">معرفی کلی </h4>
+            <p className="text-neutral8 text-xs">حداقل ۱ مورد</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => appendOverview("")}
+            className="text-primary hover:bg-primary/10 flex cursor-pointer items-center gap-1 rounded-lg px-3 py-1.5 text-sm transition-colors"
+          >
+            <MdAdd className="size-5" />
+            افزودن
+          </button>
+        </div>
+        {overviewFields.map((field, index) => (
+          <div
+            key={field.id}
+            className="mt-3 mb-2 flex w-full items-center justify-between gap-2"
+          >
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`overview-${index}`}
+                name={`features.overview.${index}`}
+                label={`متن ${toPersianNumber(index + 1)}`}
+                type="text"
+                {...register(`features.overview.${index}`)}
+              />
+            </div>
+
+            <button
+              type="button"
+              onClick={() => removeOverview(index)}
+              className="text-error hover:bg-error/10 cursor-pointer rounded-lg p-2 transition-colors"
+              disabled={overviewFields.length <= 1}
+            >
+              <MdDelete className="size-5" />
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {/* ====== Appearance ====== */}
+      <div className="border-neutral4 rounded-xl border px-4 py-2 shadow-lg">
+        <div className="flex items-center justify-between">
+          <div>
+            <h4 className="mb-2 font-medium">ویژگی‌های ظاهری</h4>
+            <p className="text-neutral8 text-xs">حداقل ۳ مورد</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => appendAppearance("")}
+            className="text-primary hover:bg-primary/10 flex cursor-pointer items-center gap-1 rounded-lg px-3 py-1.5 text-sm transition-colors"
+          >
+            <MdAdd className="size-5" />
+            افزودن
+          </button>
+        </div>
+        {appearanceFields.map((field, index) => (
+          <div
+            key={field.id}
+            className="mt-3 mb-2 flex w-full items-center justify-between gap-2"
+          >
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`appearance-${index}`}
+                name={`features.appearance.${index}`}
+                label={`متن ${toPersianNumber(index + 1)}`}
+                type="text"
+                {...register(`features.appearance.${index}`)}
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => removeAppearance(index)}
+              className="text-error hover:bg-error/10 cursor-pointer rounded-lg p-2 transition-colors"
+              disabled={appearanceFields.length <= 3}
+            >
+              <MdDelete className="size-5" />
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {/* ====== Warnings ====== */}
+      <div className="border-neutral4 rounded-xl border px-4 py-2 shadow-lg">
+        <div className="flex items-center justify-between">
+          <div>
+            <h4 className="mb-2 font-medium">نکات ویژه و هشدارها</h4>
+            <p className="text-neutral8 text-xs">حداقل ۳ مورد</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => appendWarnings("")}
+            className="text-primary hover:bg-primary/10 flex cursor-pointer items-center gap-1 rounded-lg px-3 py-1.5 text-sm transition-colors"
+          >
+            <MdAdd className="size-5" />
+            افزودن
+          </button>
+        </div>
+        {warningsFields.map((field, index) => (
+          <div
+            key={field.id}
+            className="mt-3 mb-2 flex w-full items-center justify-between gap-2"
+          >
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`warnings-${index}`}
+                name={`features.warnings.${index}`}
+                label={`متن ${toPersianNumber(index + 1)}`}
+                type="text"
+                {...register(`features.warnings.${index}`)}
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => removeWarnings(index)}
+              className="text-error hover:bg-error/10 cursor-pointer rounded-lg p-2 transition-colors"
+              disabled={warningsFields.length <= 3}
+            >
+              <MdDelete className="size-5" />
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {/* ====== Propagation ====== */}
+      <div className="border-neutral4 rounded-xl border px-4 py-2 shadow-lg">
+        <div className="flex items-center justify-between">
+          <div>
+            <h4 className="mb-2 font-medium">تکثیر</h4>
+            <p className="text-neutral8 text-xs">حداقل ۳ مورد</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => appendPropagation("")}
+            className="text-primary hover:bg-primary/10 flex cursor-pointer items-center gap-1 rounded-lg px-3 py-1.5 text-sm transition-colors"
+          >
+            <MdAdd className="size-5" />
+            افزودن
+          </button>
+        </div>
+        {propagationFields.map((field, index) => (
+          <div
+            key={field.id}
+            className="mt-3 mb-2 flex w-full items-center justify-between gap-2"
+          >
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`propagation-${index}`}
+                name={`features.propagation.${index}`}
+                label={`متن ${toPersianNumber(index + 1)}`}
+                type="text"
+                {...register(`features.propagation.${index}`)}
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => removePropagation(index)}
+              className="text-error hover:bg-error/10 cursor-pointer rounded-lg p-2 transition-colors"
+              disabled={propagationFields.length <= 3}
+            >
+              <MdDelete className="size-5" />
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {/* ====== Summary ====== */}
+      <div className="border-neutral4 rounded-xl border px-4 py-2 shadow-lg">
+        <div className="flex items-center justify-between">
+          <div>
+            <h4 className="mb-2 font-medium">جمع‌بندی</h4>
+            <p className="text-neutral8 text-xs">حداقل ۱ مورد</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => appendSummary("")}
+            className="text-primary hover:bg-primary/10 flex cursor-pointer items-center gap-1 rounded-lg px-3 py-1.5 text-sm transition-colors"
+          >
+            <MdAdd className="size-5" />
+            افزودن
+          </button>
+        </div>
+        {summaryFields.map((field, index) => (
+          <div
+            key={field.id}
+            className="mt-3 mb-2 flex w-full items-center justify-between gap-2"
+          >
+            <div className="w-full">
+              <FormField
+                icon={<MdDriveFileRenameOutline size={20} />}
+                id={`summary-${index}`}
+                name={`features.summary.${index}`}
+                label={`متن ${toPersianNumber(index + 1)}`}
+                type="text"
+                {...register(`features.summary.${index}`)}
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => removeSummary(index)}
+              className="text-error hover:bg-error/10 cursor-pointer rounded-lg p-2 transition-colors"
+              disabled={summaryFields.length <= 1}
+            >
+              <MdDelete className="size-5" />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
