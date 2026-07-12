f92242fc916a9df893711333759e185a5a22dc59
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-07-12 17:04:53 +0330

    fix(cart): update ICartItem type to support populated product object

--- src/features/cart/types/cart.types.ts
+++ src/features/cart/types/cart.types.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 export interface ICartItem {
-  product: mongoose.Types.ObjectId | string; 
+  product: mongoose.Types.ObjectId | string;
   quantity: number;
 }
 
 export interface ICart {
-  user: mongoose.Types.ObjectId | string;      
+  user: mongoose.Types.ObjectId | string;
   items: ICartItem[];
   updatedAt: Date;
 }
