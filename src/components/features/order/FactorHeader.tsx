ac9874e8dd3bc7ae4dd2bd38ac406ad45eb054ac
Ali Sadeghi <aliisadeghii192@gmail.com>
2026-07-21 15:59:20 +0330

    fix(order): use router.back in FactorHeader instead of Link

--- src/components/features/order/FactorHeader.tsx
+++ src/components/features/order/FactorHeader.tsx
@@ -1,22 +1,21 @@
+"use client"
 import SectionTitle from "@/components/panel/SectionTitle";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { MdArrowBack } from "react-icons/md";
 
-interface FactorHeaderProps{
-  href : string
-}
 
-export default function FactorHeader({href}:FactorHeaderProps) {
+export default function FactorHeader() {
+  const router = useRouter()
   return (
     <div className="flex items-center justify-between">
       <SectionTitle title="جزئیات سفارش" className="mb-0!" />
-      <Link
-        href={href}
+      <button
+        onClick={() => router.back()}
         className="flex h-10 cursor-pointer items-center justify-center gap-x-2 rounded-full bg-gray-100 px-4 text-gray-600 transition-colors hover:bg-gray-200"
       >
         <span>بازگشت</span>
         <MdArrowBack className="size-5 shrink-0" />
-      </Link>
+      </button>
     </div>
   );
 }
